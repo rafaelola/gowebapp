3a0bc93536e363c18d115a8900596b665faf6c80
Raphael Ola <follypimpz@gmail.com>
2023-08-08 21:08:11 +0100

    adding mux library --router

--- main.go
+++ main.go
@@ -3,36 +3,88 @@ package main
 import (
 	"GoWebApp/database"
 	"GoWebApp/models"
+	"GoWebApp/utils"
+	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"log"
 	"net"
 	"net/http"
-	"text/template"
+	"strconv"
 )
 
 func main() {
+	r := mux.NewRouter()
 
 	// connect to the database
 	db := database.Connect()
-	log.Println("DB Connected in main.")
-	//jsonInventories := service.GetInventory(db)
 
-	http.HandleFunc("/inventories", func(w http.ResponseWriter,
+	log.Println("DB Connected in main.")
+
+	r.HandleFunc("/inventories/all/", func(w http.ResponseWriter,
 		r *http.Request) {
 		inventories, err := models.All(db)
 		if err != nil {
 			log.Print(err)
-			http.Error(w, http.StatusText(500), 500)
+			utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		//unformattedJson := string(jsonInventories)
-		t, _ := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
-		_ = t.ExecuteTemplate(w, "T", inventories)
-		log.Println("Result html")
+		utils.RespondWithJson(w, http.StatusOK, inventories)
+		log.Println("Result inventories/all/")
 
-		log.Println("After logging inventories")
 	})
+
+	r.HandleFunc("/inventories/one/{id:[0-9]+}", func(w http.ResponseWriter,
+		r *http.Request) {
+		// get the ID from the request
+		vars := mux.Vars(r)
+		idStr := vars["id"]
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			log.Print(err)
+			utils.RespondWithError(w, http.StatusBadRequest, "Invalid ID")
+			return
+		}
+
+		inventories, err := models.One(db, id)
+		if err != nil {
+			log.Print(err)
+			utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		utils.RespondWithJson(w, http.StatusOK, inventories)
+		log.Println("Result inventories/one/")
+
+	})
+
+	r.HandleFunc("/inventories/add/", func(w http.ResponseWriter,
+		r *http.Request) {
+		// get request body and json decode it
+		decoder := json.NewDecoder(r.Body)
+		var inventory models.InventoryModel
+		err := decoder.Decode(&inventory)
+		if err != nil {
+			log.Print(err)
+			utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+			return
+		}
+		err = models.Create(db, (*models.Inventory)(&inventory))
+		if err != nil {
+			log.Print(err)
+			utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		// return the new inventory ID with 201 Created
+		utils.RespondWithJson(w, http.StatusCreated, inventory.Id)
+		log.Println("Result inventories/add/")
+
+	})
+
 	log.Println("Starting my GO server....")
+
+	http.Handle("/", r)
 
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -42,5 +94,5 @@ func main() {
 	if err != nil {
 		return // return from main
 	}
-
+	defer db.Close()
 }
